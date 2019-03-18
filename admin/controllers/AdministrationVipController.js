4ffe120ab91cd265acdbfcf7a9d29e4ad1c732ab
macawotte <maxime.pichon87@outlook.fr>
2019-03-18 15:29:02 +0100

    avancement du formulaire

--- admin/controllers/AdministrationVipController.js
+++ admin/controllers/AdministrationVipController.js
@@ -21,6 +21,21 @@ module.exports.Index = function(request, response){
       function(callback){
         model.getNationalite(function(err, result){callback(null,result)});
       },
+      function(callback){
+        model.getDefiles(function(err, result){callback(null,result)});
+      },
+      function(callback){
+        model.getFilm(function(err, result){callback(null,result)});
+      },
+      function(callback){
+        model.getMaisonDeDisque(function(err, result){callback(null,result)});
+      },
+      function(callback){
+        model.getAlbum(function(err, result){callback(null,result)});
+      },
+      function(callback){
+        model.getAgence(function(err, result){callback(null,result)});
+      },
     ],
      function(err,result){
         if (err) {
@@ -28,6 +43,11 @@ module.exports.Index = function(request, response){
             return;
         }
      response.getNationalite = result[0];
+     response.getDefiles = result[1];
+     response.getFilm = result[2];
+     response.getMaisonDeDisque = result[3];
+     response.getAlbum = result[4];
+     response.getAgence = result[5];
 
        response.render('AjouterVip', response); // appel la vue Handlebars qui va afficher le résultat
    } );
@@ -47,123 +67,3 @@ module.exports.SupprimerVip = function(request, response){
   response.title = 'Gestion des VIPs';
   response.render('SupprimerVip', response);
 };
-
-
-
-/*
-module.exports.Repertoire = function(request, response){
-   response.title = 'Répertoire des stars';
-   model.lettreNomVip(function(err, result){  // appel le module test qui exécute la requete SQL
-       if (err) {
-           console.log(err);
-           return;
-       }
-      response.lettreNomVip = result;
-
-      response.render('repertoireVips', response); // appel la vue Handlebars qui va afficher le résultat
-  } );
-
-  } ;
-
-  module.exports.RepertoireLettre = function(request, response){
-     let lettre=request.params.lettre;
-     response.title = 'Stars en '+lettre;
-     async.parallel([
-       function(callback){
-         model.lettreNomVip(function(err, result){callback(null,result)});
-       },
-       function(callback){
-         model.vipParLettre(lettre,function(err, result){callback(null,result)});
-       },
-     ],
-      function(err,result){
-         if (err) {
-             console.log(err);
-             return;
-         }
-      response.lettreNomVip = result[0];
-      response.vip = result[1];
-
-        response.render('listeVip', response); // appel la vue Handlebars qui va afficher le résultat
-    } );
-    } ;
-
-    module.exports.FicheVip = function(request, response){
-       let chiffre=request.params.chiffre;
-       response.title = 'Fiche de star';
-       async.parallel([
-         function(callback){
-           model.vipInfo(chiffre,function(err, result){callback(null,result)});
-         },
-         function(callback){
-           model.photo(chiffre,function(err, result){callback(null,result)});
-         },
-         function(callback){
-           model.mariage(chiffre,function(err, result){callback(null,result)});
-         },
-         function(callback){
-           model.liaison(chiffre,function(err, result){callback(null,result)});
-         },
-         function(callback){
-           model.mannequin(chiffre,function(err, result){callback(null,result)});
-         },
-         function(callback){
-           model.mannequinAgence(chiffre,function(err, result){callback(null,result)});
-         },
-         function(callback){
-           model.mannequinDefile(chiffre,function(err, result){callback(null,result)});
-         },
-         function(callback){
-           model.chanteur(chiffre,function(err, result){callback(null,result)});
-         },
-         function(callback){
-           model.chanteurAlbum(chiffre,function(err, result){callback(null,result)});
-         },
-         function(callback){
-           model.acteur(chiffre,function(err, result){callback(null,result)});
-         },
-         function(callback){
-           model.acteurFilm(chiffre,function(err, result){callback(null,result)});
-         },
-         function(callback){
-           model.realisateurFilm(chiffre,function(err, result){callback(null,result)});
-         },
-         function(callback){
-           model.couturierDefile(chiffre,function(err, result){callback(null,result)});
-         },
-         function(callback){
-           model.lettreNomVip(function(err, result){callback(null,result)});
-         },
-         function(callback){
-           model.nbPhoto(chiffre,function(err, result){callback(null,result)});
-         },
-       ],
-        function(err,result){
-           if (err) {
-               console.log(err);
-               return;
-           }
-
-           response.vip = result[0];
-           response.photo = result[1];
-           response.mariage = result[2];
-           response.liaison = result[3];
-           response.mannequin = result[4][0];
-           response.mannequinAgence = result[5];
-           response.mannequinDefile = result[6];
-           response.chanteur = result[7][0];
-           response.chanteurAlbum = result[8];
-           response.acteur = result[9][0];
-           response.acteurFilm = result[10];
-           response.realisateurFilm = result[11];
-           response.couturierDefile = result[12];
-           response.lettreNomVip = result[13];
-           response.nbPhoto = result[14][0].count;
-
-           //console.log(result[14][0]);
-
-          response.render('ficheVip', response);
-        }
-      );
-    };
-*/
